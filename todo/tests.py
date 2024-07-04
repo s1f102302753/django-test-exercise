ac36fa6c353ebba2512920e7f361a6ccadf4d874
1F10230275 <s1f102302753@iniad.org>
2024-07-04 11:08:20 +0900

    link with flake8

--- todo/tests.py
+++ todo/tests.py
@@ -3,10 +3,12 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
+
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample(self):
         self.assertEqual(1 + 2, 3)
+
 
 class TaskModelTestCase(TestCase):
     def test_create_task(self):
@@ -20,6 +22,7 @@ class TaskModelTestCase(TestCase):
         self.assertEqual(task.due_at, due)
 
 
+
     def test_create_task2(self):
         task = Task(title='task2')
         task.save()
@@ -28,6 +31,7 @@ class TaskModelTestCase(TestCase):
         self.assertEqual(task.title, 'task2')
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, None)
+
 
 
     def test_is_overdue_future(self):
@@ -39,6 +43,7 @@ class TaskModelTestCase(TestCase):
         self.assertFalse(task.is_overdue(current))
         
 
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
@@ -49,6 +54,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(len(response.context['tasks']), 0)
 
     
+
     def test_index_post(self):
         client = Client()
         data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
@@ -59,6 +65,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(len(response.context['tasks']), 1)
 
     
+
     def test_index_get_order_post(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
@@ -72,6 +79,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.context['tasks'][0], task2)
         self.assertEqual(response.context['tasks'][1], task1)
 
+    
     
     def test_index_get_order_due(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
@@ -87,4 +95,3 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.context['tasks'][1], task2)
 
     
-
